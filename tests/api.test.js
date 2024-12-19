87307ae6186871da4d0bc7db0a3d64be7a76357c
Matheus Lucas <nerdola.cartola@gmail.com>
2024-12-19 12:39:49 -0300

    Adding more tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,8 +1,24 @@
-const {connectDB, cleanDB} = require('../config/db');
+const { connectDB, cleanDB } = require('../config/db');
 const request = require("supertest");
 const app = require("../server");
 const mongoose = require("mongoose");
 require('dotenv').config();
+
+let user = {
+    email: "teste@gmail.com",
+    password: "pass#123"
+}
+
+const validateUser = async () => {
+    const res = await request(app)
+        .get(`/api/users/${user.email}`)
+        .set('Authorization', `Nearer ${user.acess_token}`);
+
+    expect(res.body._id).toBeDefined()
+    expect(res.body.email).toBe(user.email)
+    expect(res.body.password).toBeDefined()
+    expect(res.statusCode).toBe(200);
+}
 
 connectDB();
 cleanDB();
@@ -16,21 +32,62 @@ describe("GET /", () => {
 
 describe("POST /api/users", () => {
     it("should create a user", async () => {
-        const body = {
-            email: "teste@gmail.com",
-            password: "pass#123"
-        }
+        const body = { email, password } = user
 
         const res = await request(app).post("/api/users").send(body);
-        console.log(res.body)
         expect(res.statusCode).toBe(201);
     });
 });
 
+describe("GET /api/auth", () => {
+    it("should return a token", async () => {
+        const body = { email, password } = user
+
+        const res = await request(app).post("/api/auth").send(body);
+        expect(res.body.acess_token).toBeDefined()
+        expect(res.body.refresh_token).toBeDefined()
+        expect(res.statusCode).toBe(200);
+        user.acess_token = res.body.acess_token
+        user.refresh_token = res.body.refresh_token
+    });
+});
+
 describe("GET /api/users/:email", () => {
-    it("should return a user", async () => {
-        const res = await request(app).get("/api/users/teste@gmail.com");
+    it("should return the user that was just created", validateUser);
+});
+
+describe("PUT /api/users/:email", () => {
+    it("should update a user", async () => {
+        const body = {
+            email: "email.new@gmail.com",
+            password: "new_new_password"
+        }
+
+        const res = await request(app)
+            .put(`/api/users/${user.email}`)
+            .send(body)
+            .set('Authorization', `Nearer ${user.acess_token}`);
+
+        expect(res.statusCode).toBe(200);
+        user.email = body.email
+        user.password = body.password
+    });
+});
+
+describe("GET /api/users/:email", () => {
+    it("should return the updated user", validateUser);
+});
+
+describe("DELETE /api/auth/:email", () => {
+    it("should delete the user", async () => {
+        console.log(user)
+
+        const res = await request(app)
+            .delete(`/api/users/${user.email}`)
+            .set('Authorization', `Nearer ${user.acess_token}`);
+
         console.log(res.body)
+
         expect(res.statusCode).toBe(200);
     });
 });
